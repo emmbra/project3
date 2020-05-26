0c1ff93fea6fe3e8f4f5cef22cb763697d8b2e33
Christina Leung <cgleungsf@gmail.com>
2020-05-26 10:34:18 -0700

    added a few mascots and functionalty

--- client/src/containers/TeamContainer/createTeam/index.js
+++ client/src/containers/TeamContainer/createTeam/index.js
@@ -1,60 +1,60 @@
-import React from 'react'
-import { List, Form, Header, Button , Container} from 'semantic-ui-react'
+import React, { Component } from "react";
+import { List, Form, Header, Button, Container, Image, Grid } from 'semantic-ui-react'
+
+import mascots from "../../../static/mascots"
 
 export default (props) => {
-  return (
-    <Container textAlign='left'> 
-  <Form>
-        <Header> Create a Team!
-  </Header>
-  <subHeader> 
-    <List as='ol'>
-      <List.Item as='li' value='*'>
-        Every team can only have up to 5 members. 
-      </List.Item>
-</List>
+    console.log(mascots)
+    return (
+      <Container textAlign='left'>
+        <Form>
+          <Header>
+            Create a Team!
+          </Header>
+          <List as='ol'>
+            <List.Item as='li' value='*'>
+              Every team can only have up to 5 members.
+            </List.Item>
+          </List>
+          <Form.Field>
+            <label>Team Name: </label>
+            <input placeholder='The Lobsters' />
+          </Form.Field>
+          <Form.Group grouped>
+            <label>Team Mascot</label>
+            <Grid>
+              <Grid.Row  stretched  padded>
+                  <Button>
+                    Snake
+                    <img src={mascots.snake.src}/>
+                  </Button>
 
-  </subHeader>
-    <Form.Field>
-      <label>Team Name: </label>
-      <input placeholder='The Lobsters' />
-    </Form.Field>
+                  <Button>
+                    Tiger
+                    <img src={mascots.tiger.src}/>
+                  </Button>
 
-    {/* <Form.Field>
-      <label>Team Mascot</label>
-      <input placeholder='Will be replaced with animal emojis' />
-    </Form.Field> */}
+                  <Button>
+                    Turtle
+                    <img src={mascots.turtle.src}/>
+                  </Button>
+              </Grid.Row>
+            </Grid>
+          </Form.Group>
 
-    <Form.Group grouped>
-      <label>Team Mascot</label>
-      <Form.Field
-        label='This one'
-        icon = 'chess rock'
-        control='input'
-        type='radio'
-        name='htmlRadios'
-      />
-      <Form.Field
-        label='That one'
-        icon ='themeisle'
-        control='input'
-        type='radio'
-        name='htmlRadios'
-      />
-    </Form.Group>
+          <Form.Group grouped>
+            <label>Do you want this to be a private or public team? </label>
+            <Form.Field label='Public' control='input' type='checkbox' />
+            <Form.Field label='Private' control='input' type='checkbox' />
+            <Form.Field>
+              <label>If this is a private team, please enter a passcode for your other team members</label>
+              <input placeholder='ThePasscode' />
+            </Form.Field>
+          </Form.Group>
 
-    <Form.Group grouped>
-      <label>Do you want this to be a private or public team? </label>
-      <Form.Field label='Public' control='input' type='checkbox' />
-      <Form.Field label='Private' control='input' type='checkbox' />
-      <Form.Field>
-      <label>If this is a private team, please enter a passcode for your other team members</label>
-      <input placeholder='ThePasscode' />
-    </Form.Field>
-    </Form.Group>
+          <Button type='submit'>Submit</Button>
+        </Form>
+      </Container>
+    )
+  }
 
-    <Button type='submit'>Submit</Button>
-  </Form>
-  </Container> 
-)
-}
