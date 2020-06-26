2f89aa1a4e243ef308113da8102e42e4e21bb2eb
Tassia Shibuya <tassia.shibuya@gmail.com>
2020-06-26 11:14:11 -0700

    fixed if user has no logs yet to show the stats as 0 and dont break the page

--- client/src/containers/User/index.js
+++ client/src/containers/User/index.js
@@ -1,123 +1,159 @@
-import React, { Component } from 'react';
-import { Divider, Grid, Statistic, Table, Header, Container, } from 'semantic-ui-react';
-import { connect } from 'react-redux';
-import { compose } from 'redux';
-import { getTeamUsers } from '../../actions/team';
-import moment from 'moment';
-import { submit } from 'redux-form';
+import React, { Component } from "react";
+import {
+  Divider,
+  Grid,
+  Statistic,
+  Table,
+  Header,
+  Container,
+} from "semantic-ui-react";
+import { connect } from "react-redux";
+import { compose } from "redux";
+import { getTeamUsers } from "../../actions/team";
+import moment from "moment";
+import { submit } from "redux-form";
 
 class User extends Component {
   renderUserLogHistory = () => {
     // console.log('HELLO', this.props.getUserTeams.logs);
     if (this.props.getUserTeams.length === 0) {
-      return <Header content='No teams found!' />;
+      return <Header content="No teams found!" />;
     } else {
-      return this.props.getUserTeams.logs.map(({ timeStamp, time, distance }) => {
-        return (
-          <Table.Row >
-            <Table.Cell textAlign='center'>{moment(timeStamp).format('MMM Do YYYY')} </Table.Cell>
-            <Table.Cell textAlign='center'>{distance}</Table.Cell>
-            <Table.Cell textAlign='center'>{time}</Table.Cell>
-            <Table.Cell textAlign='center'>{distance/(time/60)}</Table.Cell>
-          </Table.Row>
-        );
-      });
+      return this.props.getUserTeams.logs.map(
+        ({ timeStamp, time, distance }) => {
+          return (
+            <Table.Row>
+              <Table.Cell textAlign="center">
+                {moment(timeStamp).format("MMM Do YYYY")}{" "}
+              </Table.Cell>
+              <Table.Cell textAlign="center">{distance}</Table.Cell>
+              <Table.Cell textAlign="center">{time}</Table.Cell>
+              <Table.Cell textAlign="center">
+                {distance / (time / 60)}
+              </Table.Cell>
+            </Table.Row>
+          );
+        }
+      );
+    }
+  };
+
+  renderUserDistance = () => {
+    if (!this.props.totalUserDistance.length) {
+      return (
+          <Statistic.Value>0</Statistic.Value>
+      );
+    } else {
+      return (
+          <Statistic.Value>
+            {this.props.totalUserDistance[0].totalDistance}
+          </Statistic.Value>
+      );
+    }
+  };
+
+  renderUserTime = () => {
+    if (!this.props.totalUserDistance.length) {
+      return (<Statistic.Value>0</Statistic.Value>);
+    } else {
+      return (
+        <Statistic.Value>
+          {this.props.totalUserDistance[0].totalTime}
+        </Statistic.Value>
+    );
     }
   };
 
   render() {
-    console.log("I'M INSIDE USER:", this.props.totalUserDistance[0]);
+    // console.log("I'M INSIDE USER:", this.props);
     // console.log("I'M INSIDE USER and GETTING TEAMS:", this.props.getUserTeams);
     // console.log('TEAM USERS INSIDE USERS:', this.props.teamUsers);
     // const numberOfTeams = this.props.getUserTeams.teams
     return (
-      <div class="dashboard" >
+      <div class="dashboard">
         <Divider
-          as='h2'
-          className='header'
+          as="h2"
+          className="header"
           horizontal
           style={{
-            color: '#858585',
-            margin: '1em 0em',
-            textTransform: 'uppercase',
+            color: "#858585",
+            margin: "1em 0em",
+            textTransform: "uppercase",
           }}
         >
           Personal Stats
         </Divider>
         <Grid>
           <Grid.Row centered columns={5}>
-            <Grid.Column textAlign='center'>
-              <Statistic size='mini'>
+            <Grid.Column textAlign="center">
+              <Statistic size="mini">
                 <Statistic.Label>Total Miles Run</Statistic.Label>
-                <Statistic.Value>{this.props.totalUserDistance[0].totalDistance}</Statistic.Value>
+                {this.renderUserDistance()}
               </Statistic>
             </Grid.Column>
-            <Grid.Column textAlign='center'>
-              <Statistic size='mini'>
+            <Grid.Column textAlign="center">
+              <Statistic size="mini">
                 <Statistic.Label>Time Spent Running</Statistic.Label>
-                  <Statistic.Value>{this.props.totalUserDistance[0].totalTime}</Statistic.Value>
+                {this.renderUserTime()}
                 <Statistic.Label>Minutes</Statistic.Label>
               </Statistic>
             </Grid.Column>
-            <Grid.Column textAlign='center'>
-              <Statistic size='mini'>
+            <Grid.Column textAlign="center">
+              <Statistic size="mini">
                 <Statistic.Label>Teams</Statistic.Label>
                 <Statistic.Value>2</Statistic.Value>
               </Statistic>
             </Grid.Column>
-            <Grid.Column textAlign='center'>
-              <Statistic size='mini'>
+            <Grid.Column textAlign="center">
+              <Statistic size="mini">
                 <Statistic.Label>Active Events</Statistic.Label>
                 <Statistic.Value>1</Statistic.Value>
               </Statistic>
             </Grid.Column>
-            <Grid.Column textAlign='center'>
-              <Statistic size='mini'>
+            <Grid.Column textAlign="center">
+              <Statistic size="mini">
                 <Statistic.Label>Events Won</Statistic.Label>
                 <Statistic.Value>1</Statistic.Value>
               </Statistic>
             </Grid.Column>
           </Grid.Row>
-
         </Grid>
 
         <Divider
-          as='h2'
-          className='header'
+          as="h2"
+          className="header"
           horizontal
           style={{
-            color: '#858585',
-            margin: '1em 0em',
-            textTransform: 'uppercase',
+            color: "#858585",
+            margin: "1em 0em",
+            textTransform: "uppercase",
           }}
         >
           Exercise Log
         </Divider>
-        <div style={{height: "200px", overflowX: "scroll", }}>
-          <Table celled selectable >
+        <div style={{ height: "200px", overflowX: "scroll" }}>
+          <Table celled selectable>
             <Table.Header>
               <Table.Row>
-                <Table.HeaderCell textAlign='center'>
-                  {' '}
-                Exercise Date{' '}
+                <Table.HeaderCell textAlign="center">
+                  {" "}
+                  Exercise Date{" "}
                 </Table.HeaderCell>
-                <Table.HeaderCell textAlign='center'>
-                  {' '}
-                Distance (miles){' '}
+                <Table.HeaderCell textAlign="center">
+                  {" "}
+                  Distance (miles){" "}
                 </Table.HeaderCell>
-                <Table.HeaderCell textAlign='center'>
-                  {' '}
-                Duration (minutes){' '}
+                <Table.HeaderCell textAlign="center">
+                  {" "}
+                  Duration (minutes){" "}
                 </Table.HeaderCell>
-                <Table.HeaderCell textAlign='center'>
-                  {' '}
-                Speed (MPH){' '}
+                <Table.HeaderCell textAlign="center">
+                  {" "}
+                  Speed (MPH){" "}
                 </Table.HeaderCell>
               </Table.Row>
             </Table.Header>
-            <Table.Body>
-              {this.renderUserLogHistory()}
-            </Table.Body>
+            <Table.Body>{this.renderUserLogHistory()}</Table.Body>
           </Table>
         </div>
       </div>
@@ -127,9 +163,8 @@ class User extends Component {
 
 function mapStateToProps(state) {
   return {
-    teamUsers: state.user
+    teamUsers: state.user,
   };
 }
 
-export default compose(
-  connect(mapStateToProps, { getTeamUsers }))(User);
+export default compose(connect(mapStateToProps, { getTeamUsers }))(User);
